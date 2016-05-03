74c5fc829d2363d0ecabda7f0729094192e67350
Marianela Piergentili <marypiergentili@gmail.com>
2016-05-03 00:45:34 -0300

    Topic 1

--- maven-project/src/main/java/bootcamp/App.java
+++ maven-project/src/main/java/bootcamp/App.java
@@ -1,5 +1,6 @@
 package bootcamp;
 
+import java.util.logging.Logger;
 
 /**
  * Hello world!
@@ -11,6 +12,12 @@ public class App
     {
         System.out.println( "Hello World!" );
         
+        //creating a logger to send an information message
+        
+        Logger log= Logger.getLogger(App.class.getName());
+        log.info("creating a singleton example");
+        
+        //using the singleton example to create and test a maven proyect with junit
         SingletonExample singletonExample = SingletonExample.getInstance();
 
 		System.out.println(singletonExample.sayHello());
--- maven-project/src/test/java/bootcamp/JUnitTest.java
+++ maven-project/src/test/java/bootcamp/JUnitTest.java
@@ -1,16 +1,18 @@
 package bootcamp;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class JUnitTest extends TestCase {
-	
-	
+import org.junit.Test;
+
+public class JUnitTest {
+
+	@Test
 	public void test() {
 		
 	       SingletonExample singletonExample = SingletonExample.getInstance();
-
+	       //junit test
 			String result = singletonExample.sayHello();
 			assertEquals("Hello", result);
-	}
 
+	}
 }
